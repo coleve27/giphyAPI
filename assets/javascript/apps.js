ca23a62a6f1f4996a4c16efcd96916ffb1d099cc
Evelyn Coleman <coleve27@gmail.com>
2017-12-04 23:01:44 -0600

    added functions from previous assignments

---
+++ assets/javascript/apps.js
@@ -0,0 +1,78 @@
+//array with topics//
+var topics = ["trump", "christmas", "coding"];
+
+//variable representing user input//
+
+var input;
+
+// TODO: function that stores user input
+
+
+//function that pushes input into topics//
+
+function addTopic() {
+  topics.push(input);
+}
+
+//calls makeButtons function//
+
+makeButtons()
+
+
+//get request from Giphy//
+// TODO: get data-name from button
+// TODO: replace "kittens" with data-value from botton clicked
+
+
+$("#cat-button").on("click", function() {
+  console.log(topics[0])
+  // Storing our giphy API URL for a random cat image
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kyHuqCiHNkqTwr677CRLL8mWyfgA7emq&q=kittens&limit=25&offset=0&rating=G&lang=en";
+
+  // Perfoming an AJAX GET request to our queryURL
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  // After the data from the AJAX request comes back
+  .done(function(response) {
+
+    // Saving the image_original_url property
+    var imageUrl = response.data[0].images.original.url;
+
+    // Creating and storing an image tag
+    var catImage = $("<img>");
+
+    // Setting the catImage src attribute to imageUrl
+    catImage.attr("src", imageUrl);
+    catImage.attr("alt", "cat image");
+
+    // Prepending the catImage to the images div
+    $("#images").prepend(catImage);
+  });
+});
+
+
+
+
+//make buttons//
+
+function makeButtons() {
+
+  $("#buttons-view").empty();
+
+  for (var i = 0; i < topics.length; i++) {
+
+    var  a = $("<button>");
+
+    a.addClass("topic");
+
+    a.attr("data-name", topics[i])
+
+    a.text(topics[i]);
+
+    $("#buttons-view").append(a);
+  }
+
+};
