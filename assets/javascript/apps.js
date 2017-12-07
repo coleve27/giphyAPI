27862b82764da22956f6599fe3db1d5f48cffd5a
Evelyn Coleman <coleve27@gmail.com>
2017-12-06 20:09:54 -0600

    cleaned up code, added ratings

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,88 +1,56 @@
 //array with topics//
 var topics = ["trump", "christmas", "coding"];
-var newTopic;
 //variable representing user input//
-
-var input;
-
-
 makeButtons()
 
-//get request from Giphy//
+$('#buttons-view').on('click', 'button', function() {
+  var topic = $(this).text();
+  // Storing our giphy API URL for a random cat image
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kyHuqCiHNkqTwr677CRLL8mWyfgA7emq&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
+  // Perfoming an AJAX GET request to our queryURL
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    // After the data from the AJAX request comes back
+    .done(function(response) {
+      $("#images").empty();
+      for (var i = 0; i < response.data.length; i++) {
 
-// TODO: replace "kittens" with data-value from botton clicked
-
-
-
-//make buttons//
+        var imageUrl = response.data[i].images.original.url;
+        var rating = response.data[i].rating;
+        // Creating and storing an image tag
+        var topicImage = $("<img>");
+        var topicDiv = $("<div>");
+        var topicRating = $("<div>");
+        topicRating.append("rating: " + rating);
+        // Setting the catImage src attribute to imageUrl
+        topicImage.attr("src", imageUrl);
+        topicImage.attr("alt", "topic image");
+        topicDiv.attr("id", "topicDiv" + i);
+        topicDiv.append(topicRating);
+        topicDiv.append(topicImage);
+        // Prepending the catImage to the images div
+        $("#images").prepend(topicDiv);
+      };
+    });
+});
 
 function makeButtons() {
-
   $("#buttons-view").empty();
-
   for (var i = 0; i < topics.length; i++) {
-
-    var  a = $("<button>");
-
+    var a = $("<button>");
     a.addClass("topic");
-
     a.attr("data-name", topics[i])
-
     a.text(topics[i]);
-
     $("#buttons-view").append(a);
   }
   return;
 };
 
-//store input from user//
-
 $("#add-topic").on("click", function(event) {
   event.preventDefault();
-  // This line grabs the input from the textbox
-  newTopic = $("#topic-input").val().trim();
-
-  // Adding movie from the textbox to our array
+  var newTopic = $("#topic-input").val().trim();
   topics.push(newTopic);
   makeButtons();
-  // Calling renderButtons which handles the processing of our movie array
-
 });
-
-//get request from Giphy//
-
-// TODO: replace "kittens" with data-value from botton clicked
-
-$("#buttons-view").on("click", function() {
-  console.log(topics[0])
-  // Storing our giphy API URL for a random cat image
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kyHuqCiHNkqTwr677CRLL8mWyfgA7emq&q=" + newTopic + "&limit=10&offset=0&rating=G&lang=en";
-
-  // Perfoming an AJAX GET request to our queryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-
-  // After the data from the AJAX request comes back
-  .done(function(response) {
-    $("#images").empty();
-    for (var i = 0; i < response.data.length; i++) {
-
-    var imageUrl = response.data[i].images.original.url;
-
-
-    // Creating and storing an image tag
-    var catImage = $("<img>");
-
-    // Setting the catImage src attribute to imageUrl
-    catImage.attr("src", imageUrl);
-    catImage.attr("alt", "cat image");
-
-    // Prepending the catImage to the images div
-  $("#images").prepend(catImage);
-  };
-});
-
-});
-  // Adding movie from the textbox to our array
